387f110aaf777cebce6b9248be0dd60ae4a5e069
owen-gervais <60107914+owen-gervais@users.noreply.github.com>
2021-07-09 15:24:46 -0400

    Update getTemps.py

--- 3D-Printer-Control-Hub/scripts/getTemps.py
+++ 3D-Printer-Control-Hub/scripts/getTemps.py
@@ -3,19 +3,59 @@ import os
 
 def getTemps() -> bool: 
 
-base = "http://octopi.local"
-url = "/api/printer"
-query ="?apikey="
-apikey = os.getenv("OCTOPI_KEY")
+    # URL Componnents
+    base = "http://octopi.local"
+    url = "/api/printer"
+    
+        # Defining the headers 
+    headers = {
+        'X-Api-Key': os.getenv("OCTOPI_KEY"),
+        'Content-Type': "application/json"
+        }
 
-reqString = base+url+query+apikey 
+    query ="?apikey="
+    apikey = os.getenv("OCTOPI_KEY")
 
-try:
-    r = requests.get(reqString)
-    prntrStatus = r.json()
-    bedTemp = prntrStatus['temperature']['bed']['actual']
-    extrTemp = prntrStatus['temperature']['tool0']['actual']
-    print('BedTemp={0:0.1f} ExtrTemp={1:0.1f}'.format(bedTemp,extrTemp))
-except:
-    print('Request Failed');
-    pass
+    reqString = base+url+query+apikey 
+
+    try:
+        r = requests.get(reqString)
+        
+        
+        
+        prntrStatus = r.json()
+        bedTemp = prntrStatus['temperature']['bed']['actual']
+        extrTemp = prntrStatus['temperature']['tool0']['actual']
+        print('BedTemp={0:0.1f} ExtrTemp={1:0.1f}'.format(bedTemp,extrTemp))
+        return True
+    except:
+        print('Request Failed');
+        return False
+
+    
+    
+     # URL Componnents
+    base = "http://octopi.local"
+    url = "/api/job"
+
+    # Defining the headers 
+    headers = {
+        'X-Api-Key': os.getenv("OCTOPI_KEY"),
+        'Content-Type': "application/json"
+        }
+
+    # Defining the payload
+    payload = {
+      "command": "cancel"
+    }
+
+    # Defining request parameters
+    params = {}
+
+    # Calling the tool temp endpoint
+    try:
+        response = requests.request("GET",
+                                    base+url,
+                                    params=params,
+                                    headers=headers)
+        return True
